ccf3d18a0f9073323925307701cd1c58637121e0
DerAaal <fnpetergl@gmx.de>
2023-05-19 01:56:10 +0200

    Ja

---
+++ Coop-Stuff/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _life;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _range;
+    
+    private Rigidbody _rigidbody;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(gameObject.transform.position, _player.transform.position) <=_range) 
+        {
+            Vector3 direction = _player.transform.position - gameObject.transform.position;
+            direction.Normalize();
+            transform.LookAt(_player.transform.position);
+            _rigidbody.AddForce(direction * _speed);
+
+        }
+    }
+
+
+       
+}
